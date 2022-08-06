e7898de5be8d0680da313341944afc59078dbd92
Bilal Atrgouti <90733226+atrgouti@users.noreply.github.com>
2022-08-06 02:06:32 +0100

    added media query for responsive content

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const lists = document.querySelectorAll('.lists')
 const nav = document.querySelector('.nav')
-
-
+const linksWrapper = document.querySelector('.links-wraper')
+const humberger = document.querySelector('.humberger') 
+const humbo = document.querySelector('.humbergr-men') 
+const humbergrMenu = document.querySelector('.humbergr-menu')
+const closeIcon = document.querySelector('.close-icon')
 lists.forEach((list) => {
     list.addEventListener('click', () => {
         list.classList.toggle('active')
     })
+})
+
+
+humberger.addEventListener('click', () => {
+    linksWrapper.classList.add('active')
+    humbergrMenu.classList.add('active')
+})
+closeIcon.addEventListener('click', () => {
+    linksWrapper.classList.remove('active')
+    humbergrMenu.classList.remove('active')
 })
 
 
